3aca5ba9f9ec12a2706196912f39e598afcd2dfe
Goppot <78550283+Goppot@users.noreply.github.com>
2021-07-04 23:46:33 +0300

    Add files via upload

---
+++ src/main/java/App.java
@@ -0,0 +1,24 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        
+        Cat one = applicationContext.getBean("cat", Cat.class);
+        System.out.println(one.getMassage());
+
+        Cat two = applicationContext.getBean("cat", Cat.class);
+        System.out.println(two.getMassage());
+
+        System.out.println(bean.equals(bean1));
+        System.out.println(bean.equals(one));
+    }
+}
---
+++ src/main/java/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getMassage() {
+        return name;
+    }
+
+    public void setMassage(String name) {
+        this.name = name;
+    }
+}
